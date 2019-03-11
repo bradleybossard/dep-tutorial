525a9571fa8ff2b96ad546dc680af8b5e0af26ea
Bradley Bossard <bradleybossard@gmail.com>
2019-03-11 17:46:48 -0500

    Add pkg/errors to build

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
 import "fmt"
+import "io/ioutil"
+import "github.com/pkg/errors"
+
+func readFile() ([]byte, error) {
+	dat, err := ioutil.ReadFile("./test.tmp")
+	if err != nil {
+		fmt.Println("Unwrapped = ", err)
+		return dat, errors.Wrap(err, "read failed")
+	}
+	return dat, err
+}
 
 func main() {
-	fmt.Println("vim-go")
+	_, err := readFile()
+	if err != nil {
+		fmt.Println("Wraped = ", err)
+	}
 }
